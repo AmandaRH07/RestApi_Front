f61f9357826f37c91430c68833759a3110865cb4
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2022-12-06 20:58:12 -0300

    Login structure page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
+import React from 'react';
+import './global.css'
+import Login from './pages/Login';
+
+import Router from './router'
 
 export default function App() {
-  return ( 
-    <h1>Hello world</h1>
+  return (
+    <div>
+      <Router />
+    </div>
   );
 }
---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+import padlock from '../../assets/padlock.png'
+
+export default function Login(){
+  return(
+   <div className="login-container">
+    <div className="login-form">
+      <img className="login-padlock"src={padlock} alt="Login"/>
+      <form>
+        <h1>Login</h1>
+        <input placeholder="Usuário" type="text"/>
+        <input placeholder="Senha" type="password"/>
+        <button className="button" type="submit">Salvar</button> 
+      </form>
+    </div>
+   </div>
+  );
+}
