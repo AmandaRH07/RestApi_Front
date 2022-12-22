dec85fd7806aba11c8079f180e31d117eee7c5a9
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2022-12-22 12:01:49 -0300

    Integrating backend login and new books

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,19 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+import api from '../../services/api';
 import './styles.css';
 import padlock from '../../assets/padlock.png'
 
-export default function Login(){
-  return(
-   <div className="login-container">
-    <div className="login-form">
-      <img className="login-padlock"src={padlock} alt="Login"/>
-      <form>
-        <h1>Login</h1>
-        <input placeholder="Usuário" type="text"/>
-        <input placeholder="Senha" type="password"/>
-        <button className="button" type="submit" style={{marginTop: '6%' }}>Save</button> 
-      </form>
+export default function Login() {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  async function login(e) {
+    e.preventDefault();
+
+    const data = {
+      userName,
+      password
+    };
+
+    try {
+      const response = await api.post('api/Auth/v1/signin', data);
+
+      localStorage.setItem('userName', userName);
+      localStorage.setItem('accessToken', response.data.accessToken );
+      localStorage.setItem('refreshToken', response.data.refreshToken);
+
+      navigate('/books');
+    }
+    catch (error) {
+      alert("Login failed, please try again!");
+    }
+  }
+
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <img className="login-padlock" src={padlock} alt="Login" />
+        <form onSubmit={login}>
+          <h1>Login</h1>
+          <input
+            placeholder="User"
+            type="text"
+            value={userName}
+            onChange={e => setUserName(e.target.value)}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <button className="button" type="submit" style={{ marginTop: '6%' }}>Save</button>
+        </form>
+      </div>
     </div>
-   </div>
   );
 }
--- client/src/pages/NewBook/index.js
+++ client/src/pages/NewBook/index.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
+import api from '../../services/api';
 import './styles.css';
 
 export default function NewBook() {
+  const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState('');
+  const [lauchDate, setLauchDate] = useState('');
+  const [price, setPrice] = useState('');
+
+  const navigate = useNavigate();
+
+  async function createNewBook(e) {
+    e.preventDefault();
+
+    const data = {
+      author,
+      title,
+      lauchDate,
+      price,
+    };
+
+    const accessToken = localStorage.getItem('accessToken');
+
+    try {
+      await api.post('api/Book/v1', data, {
+        headers:{
+          Authorization: `Bearer ${accessToken}` 
+        }
+      });
+
+      navigate('/books');
+    }
+    catch (error) {
+      alert("Falied creating new book, please try again!" + error);
+    }
+  }
+
   return (
     <div className='new-book-container'>
       <div className='content'>
@@ -11,15 +46,31 @@ export default function NewBook() {
           <h1>Add New Book</h1>
           <p>Enter the book information and click on 'Add'!</p>
           <Link className='back-link' to="/books">
-            <FiArrowLeft size={16} color="154c79"/>
+            <FiArrowLeft size={16} color="154c79" />
             Home
           </Link>
         </section>
-        <form>
-          <input placeholder='Title'/>
-          <input placeholder='Author'/>
-          <input type='Date'/>
-          <input placeholder='Price'/>
+        <form onSubmit={createNewBook}>
+          <input
+            placeholder='Title'
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <input
+            placeholder='Author'
+            value={author}
+            onChange={e => setAuthor(e.target.value)}
+          />
+          <input
+            type='Date'
+            value={lauchDate}
+            onChange={e => setLauchDate(e.target.value)}
+          />
+          <input
+            placeholder='Price'
+            value={price}
+            onChange={e => setPrice(e.target.value)}
+          />
 
           <button className='button' type="submit">Add</button>
         </form>
